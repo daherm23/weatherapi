3adabde9dc6f47290c0111ef335af22372bb9bae
Mo <daher.mohanad@gmail.com>
2018-03-22 13:58:27 -0700

    first

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let path = require('path')
 let app = express()
 
 
-app.use(express.static( __dirname + '/weather/dist' ));
+app.use(express.static(path.join( __dirname + '/weather/dist' )));
 
 app.listen(8000, function() {
     console.log('listening on port 8000');
